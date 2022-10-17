42651efea1c40919f92091a9141bd9526307ae2e
Nithaparan Francis <nithaparanfrancis@Nithaparans-MacBook-Pro.local>
2022-10-17 19:11:41 -0400

    Initial project

---
+++ HomeStars-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeStars-clone
+//
+//  Created by Nithaparan Francis on 2022-10-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
